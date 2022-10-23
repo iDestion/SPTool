13a1c529452bcc7280227ab562de171045e7e2e7
iDestion <mick.sneekes@live.nl>
2022-10-23 16:19:11 +0200

    Added functionality for compensating for speaker turn length when searching for terms.

--- src/Searcher.java
+++ src/Searcher.java
@@ -5,16 +5,20 @@ import java.util.HashMap;
 public class Searcher {
 
     //Returns a map with speakerturns and their respective scores according to the amount of occurrences of the terms.
-    public static HashMap<SpeakerTurn, Integer> multiTermFrequency(ArrayList<SpeakerTurn> input, ArrayList<String> terms){
-        HashMap<SpeakerTurn, Integer> result = new HashMap<>();
+    public static HashMap<SpeakerTurn, Double> multiTermFrequency(ArrayList<SpeakerTurn> input, ArrayList<String> terms, boolean compensateLength){
+        HashMap<SpeakerTurn, Double> result = new HashMap<>();
 
         //For each speaker turn, look at the simple words that are in the turn, count the frequency of the searchterms, this is the score for the specific turn. Possibility for accounting for longer terms.
         for(SpeakerTurn turn : input){
-            int score = 0;
+            double score = 0;
             for(String term : terms){
                 score += Collections.frequency(turn.getWords(), term);
             }
-            result.put(turn, score);
+            if(compensateLength){
+                result.put(turn, score/turn.getWordCount());
+            } else {
+                result.put(turn, score);
+            }
         }
 
         return result;
