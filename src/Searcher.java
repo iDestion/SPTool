f6574e74e2496c4bc0f996d35c3073b1514e430e
iDestion <mick.sneekes@live.nl>
2022-10-23 15:39:34 +0200

    Changed name of search to something more descriptive.

--- src/Searcher.java
+++ src/Searcher.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 public class Searcher {
 
     //Returns a map with speakerturns and their respective scores according to the amount of occurrences of the terms.
-    public static HashMap<SpeakerTurn, Integer> dumbSearch(ArrayList<SpeakerTurn> input, ArrayList<String> terms){
+    public static HashMap<SpeakerTurn, Integer> multiTermFrequency(ArrayList<SpeakerTurn> input, ArrayList<String> terms){
         HashMap<SpeakerTurn, Integer> result = new HashMap<>();
 
         //For each speaker turn, look at the simple words that are in the turn, count the frequency of the searchterms, this is the score for the specific turn. Possibility for accounting for longer terms.
