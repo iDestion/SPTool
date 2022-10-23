93148621bdfc64fe34e44000f8481dab7e78418a
iDestion <mick.sneekes@live.nl>
2022-10-23 15:33:51 +0200

    First version of search (dumbsearch), purely based on searchterm frequency, added.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 
 public class Main {
 
@@ -10,8 +13,9 @@ public class Main {
 
         try {
             ArrayList<SpeakerTurn> turns = Utils.splitTurns(file);
-            for(SpeakerTurn turn : turns) {
-                System.out.println(turn.getWords());
+            HashMap<SpeakerTurn, Integer> scores = Searcher.dumbSearch(turns, new ArrayList<String>(List.of("dan", "overzicht")));
+            for(SpeakerTurn turn : scores.keySet()){
+                System.out.println("Speakerturn text: " + turn.getText() + "\n Score: " + scores.get(turn));
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
---
+++ src/Searcher.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+
+public class Searcher {
+
+    //Returns a map with speakerturns and their respective scores according to the amount of occurrences of the terms.
+    public static HashMap<SpeakerTurn, Integer> dumbSearch(ArrayList<SpeakerTurn> input, ArrayList<String> terms){
+        HashMap<SpeakerTurn, Integer> result = new HashMap<>();
+
+        //For each speaker turn, look at the simple words that are in the turn, count the frequency of the searchterms, this is the score for the specific turn. Possibility for accounting for longer terms.
+        for(SpeakerTurn turn : input){
+            int score = 0;
+            for(String term : terms){
+                score += Collections.frequency(turn.getWords(), term);
+            }
+            result.put(turn, score);
+        }
+
+        return result;
+    }
+
+}
